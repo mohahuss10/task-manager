9577015b1ebdbe37b54ef9d4f406e9d68e1a636a
mohamed <mohamedhussein6500@gmail.com>
2026-03-07 20:59:25 +0300

    add task routes, controllers, mongodb connection and task model

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('Task Manager App')
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']); 
+const express = require('express')
+const tasks = require('./routes/tasks')
+const connectDb = require('./db/connect')
+require('dotenv').config()
+
+const app = express()
+
+
+//middlewares
+app.use(express.json())
+//routes
+
+app.get('/hello',(req,res)=>{
+    res.send('Task manager')
+
+
+
+})
+app.use('/api/v1/tasks',tasks)
+
+
+
+
+
+
+const port   = 4000
+
+ const start = async()=>{
+    try {
+        await connectDb(process.env.MONGO_URI)
+        app.listen(port,console.log(`server is listening on port ${port} .....`))
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+ }
+
+
+ start()
---
+++ controllers/tasks.js
@@ -0,0 +1,28 @@
+const getAllTasks = (req,res)=>{
+    res.send('get all items')
+}
+
+const getTask = (req,res)=>{
+    res.send('get single task')
+}
+const createTask = (req,res)=>{
+    res.send('create task')
+}
+const updateTask = (req,res)=>{
+    res.send('update task')
+}
+
+const deleteTask = (req,res)=>{
+    res.send('delete task')
+}
+
+
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    updateTask,
+    getTask,
+    deleteTask,
+
+}
