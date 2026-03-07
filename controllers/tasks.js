30bd7d38f4a24371b47d5d9faee268c01beadaaf
mohamed <mohamedhussein6500@gmail.com>
2026-03-07 21:54:59 +0300

    added validation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,3 +1,4 @@
+const Task =  require('../models/Task')
 const getAllTasks = (req,res)=>{
     res.send('get all items')
 }
@@ -5,8 +6,9 @@ const getAllTasks = (req,res)=>{
 const getTask = (req,res)=>{
     res.send('get single task')
 }
-const createTask = (req,res)=>{
-    res.send('create task')
+const createTask = async (req,res)=>{
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
 }
 const updateTask = (req,res)=>{
     res.send('update task')
--- models/Task.js
+++ models/Task.js
@@ -1,5 +1,21 @@
+const { string } = require('i/lib/util')
 const mongoose   =require('mongoose')
 
 
 
-const TaskSchema = new mongoose.Schema()
+const TaskSchema = new mongoose.Schema({
+
+    //basic validation....
+    name:{
+    type:String,
+    required:[true,'must provide a name '],
+    trim:true,
+    maxlength:[20,'name cannot be more than 20 characters']
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
+})
+
+module.exports = mongoose.model('Task',TaskSchema)
