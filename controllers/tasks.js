7186dbf5c405d8375ea462e3fb8ef9b6709654d0
mohamed <mohamedhussein6500@gmail.com>
2026-03-08 08:20:15 +0300

    implement getAllTasks controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,14 +1,27 @@
 const Task =  require('../models/Task')
-const getAllTasks = (req,res)=>{
-    res.send('get all items')
+const getAllTasks = async(req,res)=>{
+    try {
+        const tasks  = await Task.find({})
+        res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+
 }
 
 const getTask = (req,res)=>{
     res.send('get single task')
 }
 const createTask = async (req,res)=>{
+    try {
     const task = await Task.create(req.body)
     res.status(201).json({task})
+        
+    } catch (error) {
+        res.status(500).json({msg:error})
+        
+    }
+ 
 }
 const updateTask = (req,res)=>{
     res.send('update task')
